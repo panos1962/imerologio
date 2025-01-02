5013c29ca94dab0ed0596d9376f73c8e2387df1e
panos1962 <panos1962@gmail.com>
2025-01-02 18:29:00 +0200

    modifications

--- www/index.php
+++ www/index.php
@@ -9,6 +9,7 @@ define("ETOS_MAX", 9993);
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Anton+SC&family=Paytone+One&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="imerologio.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="imerologio.js"></script>
